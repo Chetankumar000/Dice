e2e14fe2251e2358d5afc8c6a90f1138b14937fa
Chetankumar Mistry <86110419+Chetankumar000@users.noreply.github.com>
2021-08-20 10:28:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+var randomNumber1=Math.floor((Math.random()*6)+1);
+function random(){
+    if (randomNumber1===1){
+        document.querySelector("img").setAttribute("src","images/dice1.png");
+    }
+    else if(randomNumber1===2){
+        document.querySelector("img").setAttribute("src","images/dice2.png");
+    }
+    else if(randomNumber1===3){
+        document.querySelector("img").setAttribute("src","images/dice3.png");
+    }
+    else if(randomNumber1===4){
+        document.querySelector("img").setAttribute("src","images/dice4.png");
+    }
+    else if(randomNumber1===5){
+        document.querySelector("img").setAttribute("src","images/dice5.png");
+    }
+    else if(randomNumber1===6){
+        document.querySelector("img").setAttribute("src","images/dice6.png");
+    }
+}
+
+var randomNumber2=Math.floor((Math.random()*6)+1);
+function random2(){
+    if (randomNumber2===1){
+        document.querySelectorAll("img")[1].setAttribute("src","images/dice1.png");
+    }
+    else if(randomNumber2===2){
+        document.querySelectorAll("img")[1].setAttribute("src","images/dice2.png");
+    }
+    else if(randomNumber2===3){
+        document.querySelectorAll("img")[1].setAttribute("src","images/dice3.png");
+    }
+    else if(randomNumber2===4){
+        document.querySelectorAll("img")[1].setAttribute("src","images/dice4.png");
+    }
+    else if(randomNumber2===5){
+        document.querySelectorAll("img")[1].setAttribute("src","images/dice5.png");
+    }
+    else if(randomNumber2===6){
+        document.querySelectorAll("img")[1].setAttribute("src","images/dice6.png");
+    }
+}
+function heading(){
+    if(randomNumber1 > randomNumber2){
+        document.querySelector("h1").innerHTML=" 🎉🎇Player 1 Wins";
+        }
+    else if(randomNumber1 < randomNumber2){
+        document.querySelector("h1").innerHTML=" Player 2 Wins🎉🎇";
+        }
+    else if(randomNumber1 === randomNumber2){
+        document.querySelector("h1").innerHTML=" DRAW🎉🎇";
+        }
+}
+random();
+random2();
+heading();
